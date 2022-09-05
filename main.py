2f269973cb726ad763d75aa1f091e3262be79f10
Andrey Rodriguez <andrey.gz.2016@gmail.com>
2022-09-04 20:25:48 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+
+
+numero_telefono = "+5354674826"
+
+pywhatkit.sendwhatmsg(numero_telefono, "Hola, este es un mensaje de prueba", 8, 12,15,True,10)
+
+println("Mensaje enviado correctamente")
